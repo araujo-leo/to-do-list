cc0cc7a66d649c6a9932d1dd5c9d8b62a274e140
araujo-leo <leoaraujofnd@gmail.com>
2024-09-12 20:47:35 -0300

    fix: Corrigindo códigos repetidos

--- backend/controllers/TaskController.php
+++ backend/controllers/TaskController.php
@@ -1,7 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-
 include_once '../config/Database.php';
 include_once '../models/TaskModel.php';
 
