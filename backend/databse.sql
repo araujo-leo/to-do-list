a625c8979e54ae39029b61a6de947e9e833aed91
araujo-leo <leoaraujofnd@gmail.com>
2024-09-23 23:08:09 -0300

    fix: Alterando sql da tabela

--- backend/databse.sql
+++ backend/databse.sql
@@ -1,24 +1,36 @@
 CREATE DATABASE todolist;
 USE todolist;
 
-CREATE TABLE todos (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-    status ENUM('pendende', 'em progresso', 'completa') DEFAULT 'pendende',
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-select * from todos;
-
-insert into todos (name) VALUES ('tarefa 1');
-
-
-CREATE TABLE users(
-	id INT auto_increment primary KEY,
-    name varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
+CREATE TABLE todos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+    status ENUM('pendente', 'em progresso', 'completa') DEFAULT 'pendente',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    jwt_token varchar(512) DEFAULT NULL
+    user_id INT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE token (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    token VARCHAR(512) NOT NULL,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id)
+        REFERENCES users (id)
+        ON DELETE CASCADE
+);
+
+select * from todos;
+select * from users;
+select * from token;
