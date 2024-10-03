c0b236120fc5e11a4e5b7ced013193205d0911f7
ZekNogueira <emelonogueira35@gmail.com>
2024-10-03 17:37:13 -0300

    feat: logout implemented

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,3 +1,4 @@
+
 console.log(localStorage.getItem('user_token'));
 
 document.addEventListener('DOMContentLoaded', verify_session());
@@ -42,6 +43,7 @@ function verify_logout(data) {
 
             if (result.status === 'success') {
                 window.location.href = 'pages/login-sigin.html';
+                localStorage.removeItem('user_token');
                 console.log('Sucesso');
             } else {
                 console.log("erro:", result.message);
@@ -55,65 +57,6 @@ function verify_logout(data) {
 
    
 
-    
-/* 
-
-document.getElementById('sigin-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const new_user_name = document.getElementById('new-user-name').value;
-    const new_user_password = document.getElementById('new-user-password').value;
-    const new_user_email = document.getElementById('new-user-email').value;
-
-    sigin(new_user_name, new_user_email, new_user_password);
-});
-
-
-function sigin(user_name, user_email, user_password) {
-    const data = {
-        name: user_name,
-        email: user_email,
-        password: user_password
-    };
-
-
-    fetch('http://localhost/to-do-list/backend/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(result => {
-            // erro: sempre caindo no catch mas está registrando.
-            if (result.status === 'success') {
-                alert("Usuário registrado com sucesso!");
-            } else {
-                console.log("Erro ao registrar o usuário:", result.message);
-                alert("Erro ao registrar: " + result.message);
-            }
-        })
-        .catch(error => {
-            // erro console.log('Erro na requisição:', error);
-
-        });
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-*/
 
 
 
--- frontend/scripts/login-sigin.js
+++ frontend/scripts/login-sigin.js
@@ -3,16 +3,25 @@ document.addEventListener('COMContentLoaded', listeners());
 
 function listeners() {
     const login_form = document.getElementById('login-form');
-    login_form.addEventListener('submit', function (e){
+    login_form.addEventListener('submit', function (e) {
         e.preventDefault();
         login();
     });
 
 
 
+    const register_form = document.getElementById('sigin-form');
+    register_form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        register();
+
+
+    });
 }
 
- //LOGIN
+
+
+//LOGIN
 function login() {
 
 
@@ -61,6 +70,61 @@ function enter_login() {
 function archive_login_token(token) {
     localStorage.setItem('user_token', token);
 }
+
+
+
+
+
+
+
+//register
+
+
+function register() {
+    const data = {
+        name: document.getElementById('new-user-name').value,
+        email: document.getElementById('new-user-email').value,
+        password: document.getElementById('new-user-password').value
+    };
+
+
+    fetch('http://localhost/to-do-list/backend/api/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(result => {
+            
+
+            if (result.status === 'success') {
+                alert("Usuário registrado com sucesso!");
+            } else {
+                console.log("Erro ao registrar o usuário:", result.message);
+                alert("Erro ao registrar: " + result.message);
+            }
+        })
+        .catch(error => {
+            console.log('Erro na requisição:', error);
+            console.log(result.status);
+        });
+        
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
